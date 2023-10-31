91d845cd8fcac32a915d6831163a0cac15be06cd
Danner Baumgartner <danner.baumgartner@gmail.com>
2023-10-30 19:09:28 -0700

    Passed all tests

--- lib/employee.py
+++ lib/employee.py
@@ -20,6 +20,40 @@ class Employee:
             f"Department ID: {self.department_id}>"
         )
 
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self,name):
+        if isinstance(name,str) and len(name) > 0:
+            self._name = name
+        else:
+            raise ValueError
+        
+    @property
+    def job_title(self):
+        return self._job_title
+    
+    @job_title.setter
+    def job_title(self,job_title):
+        if isinstance(job_title,str) and len(job_title) > 0:
+            self._job_title = job_title
+        else:
+            raise ValueError
+        
+    @property
+    def department_id(self):
+        return self._department_id
+
+    @department_id.setter
+    def department_id(self, department_id):
+        if type(department_id) is int and Department.find_by_id(department_id):
+            self._department_id = department_id
+        else:
+            raise ValueError(
+                "department_id must reference a department in the database")
+
     @classmethod
     def create_table(cls):
         """ Create a new table to persist the attributes of Employee instances """
